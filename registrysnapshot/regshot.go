7ab5e610205ac7c921fcd84a2b5e277aa9dcaa52
1d8 <56985822+1d8@users.noreply.github.com>
2022-10-26 09:07:54 -0700

    Add files via upload

---
+++ registrysnapshot/regshot.go
@@ -0,0 +1,38 @@
+package registrysnapshot
+
+import (
+	"golang.org/x/sys/windows/registry"
+	"wingoEDR/logger"
+)
+
+type RegistryValues struct {
+	registryFullPath	string
+	registryValue		string
+}
+
+// supply registry path & the keys you want from those paths
+// slices must be of equal length
+// EX: GetRegistryValues([]string{"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"}, []string{"SystemRoot"})
+func GetRegistryValue(registryPathSlice []string, registryKeySlice []string) []RegistryValues {
+	var regValues []RegistryValues
+	if len(registryPathSlice) != len(registryKeySlice) {
+		zap.S().Fatal("Registry path slice isn't equal to registry key slice!")
+	}
+	for index, registryPath := range registryPathSlice {
+		keyHandle, err := registry.OpenKey(registry.LOCAL_MACHINE, registryPath, registry.QUERY_VALUE)
+		if err != nil {
+			zap.S().Warn("Cannot open registry key: " + registryPath + err.Error())
+		}
+		keyValue, _, err := keyHandle.GetStringValue(registryKeySlice[index])
+		if err != nil {
+			/zap.S().Warn("Cannot get value for key: " + registryKeySlice[index])
+		} else {
+			
+			keyValuePair := RegistryValues{registryPath, keyValue}
+			regValues = append(regValues, keyValuePair)
+		}
+		keyHandle.Close()
+	}
+	return regValues
+}
+
