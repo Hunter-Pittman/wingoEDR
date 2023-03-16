548e4ca95927459ffe727779eae154b20796edc3
1d8 <bnu1s@protonmail.com>
2023-03-15 23:24:55 -0700

    Combined software module to registrycapture module (GetSoftwareSubkeys function)

--- registrycapture/regshot.go
+++ registrycapture/regshot.go
@@ -12,6 +12,7 @@ type RegistryValues struct {
 	registryValue    string
 }
 
+
 // supply registry path & the keys you want from those paths
 // slices must be of equal length
 // EX: GetRegistryValues([]string{"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"}, []string{"SystemRoot"})
@@ -19,8 +20,8 @@ func GetRegistryValue(registryPathSlice []string, registryKeySlice []string) []R
 	var regValues []RegistryValues
 	if len(registryPathSlice) != len(registryKeySlice) {
 		zap.S().Error("Registry path slice isn't equal to registry key slice!")
-		//zap.S().Fatal("Registry path slice isn't equal to registry key slice!")
 	}
+
 	for index, registryPath := range registryPathSlice {
 		keyHandle, err := registry.OpenKey(registry.LOCAL_MACHINE, registryPath, registry.QUERY_VALUE)
 		if err != nil {
@@ -39,6 +40,59 @@ func GetRegistryValue(registryPathSlice []string, registryKeySlice []string) []R
 		keyHandle.Close()
 	}
 	return regValues
+}
+
+type InstalledSoftware struct {
+	Name    string
+	Version string
+	InstallPath	string
+	Publisher	string
+	UninstallString	string
+}
+
+// Return subkey values for installed software
+func GetSoftwareSubkeys(registryPath string) []InstalledSoftware {
+
+	keyHandle, err := registry.OpenKey(registry.LOCAL_MACHINE, registryPath, registry.QUERY_VALUE|registry.ENUMERATE_SUB_KEYS)
+	if err != nil {
+		zap.S().Error(err)
+	}
+	subkeys, err := keyHandle.ReadSubKeyNames(-1)
+	if err != nil {
+		zap.S().Error(err)
+	}
+	
+	softwareList := []InstalledSoftware{}
+	for _, subkey := range subkeys {
+		subKeyHandle, err := registry.OpenKey(registry.LOCAL_MACHINE, registryPath +`\`+subkey, registry.QUERY_VALUE)
+		if err != nil {
+			zap.S().Error(err)
+		}
+		publisher, _, err := subKeyHandle.GetStringValue("Publisher")
+		if err != nil {
+			zap.S().Error(err)
+		}
+		installLocation, _, err := subKeyHandle.GetStringValue("InstallLocation")
+		if err != nil {
+			zap.S().Error(err)
+		}
+		uninstallString, _, err := subKeyHandle.GetStringValue("UninstallString")
+		if err != nil {
+			zap.S().Error(err)
+		}
+		displayName, _, err := subKeyHandle.GetStringValue("DisplayName")
+		if err != nil {
+			zap.S().Error(err)
+		}
+		version, _, err := subKeyHandle.GetStringValue("DisplayVersion")
+		if err != nil {
+			zap.S().Error(err)
+		}
+		softwareInfo := InstalledSoftware{displayName, version, installLocation, publisher, uninstallString}
+		softwareList = append(softwareList, softwareInfo)
+	}
+	keyHandle.Close()
+	return softwareList
 }
 
 func GetLastWriteTime(keyHandle registry.Key) time.Time {
